783bd0e9ac4001d798502e7705dbb49ca21e1c35
Sammmte <santiagoperez.-@hotmail.com>
2017-06-26 18:43:45 +0200

    ..

---
+++ LunarLander/Assets/Resources/Scripts/SoundScripts/ComplexAudio.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ComplexAudio : MonoBehaviour{
+
+    AudioSource source;
+
+    [SerializeField]
+    string clipName;
+
+    public void Init()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public AudioSource GetAudioSource()
+    {
+        return source;
+    }
+
+    public string GetClipName()
+    {
+        return clipName;
+    }
+
+}
